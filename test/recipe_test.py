d611931e1371d2dd58c32874fde61cb6d6673610
will-mcn <wam59@georgetown.edu>
2022-05-05 19:02:44 -0400

    Update recipe_test.py

--- test/recipe_test.py
+++ test/recipe_test.py
@@ -4,6 +4,7 @@ import os
 import pytest
 from app.recipe_service import get_recipes
 
+# testing to ensure we can properly access the API and access necessary data
 def test_get_recipes():
     results = get_recipes(type_of_meal="pasta",final_ingreds="pasta+salmon", minimum_calories="200", maximum_calories="1000", max_ready_time="60")
     #recipe_results = [recipe for recipe in results["results"]]
@@ -12,7 +13,7 @@ def test_get_recipes():
     assert results[0]["readyInMinutes"] == 45
     assert results[0]["nutrition"]["nutrients"][0]["amount"] == 473.939
     assert results[0]["aggregateLikes"] == 33
-    
+
 
 
 
