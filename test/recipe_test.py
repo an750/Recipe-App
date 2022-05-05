7495c88b71ec317837c4b0f46b178d7d92406639
will-mcn <wam59@georgetown.edu>
2022-05-05 15:54:06 -0400

    finalized testing logic

--- test/recipe_test.py
+++ test/recipe_test.py
@@ -2,18 +2,22 @@
 
 import os 
 import pytest
-
 from app.recipe_service import get_recipes
 
 def test_get_recipes():
     results = get_recipes(type_of_meal="pasta",final_ingreds="pasta+salmon", minimum_calories="200", maximum_calories="1000", max_ready_time="60")
-    recipe_results = [recipe for recipe in results["results"]]
-    assert recipe_results[0]["type_of_meal"] == "pasta"
-    assert recipe_results[0]["final_ingreds"] == "pasta+salmon"
-    assert recipe_results[0]["minimum_calories"] == "200"
-    assert recipe_results[0]["maximum_calories"] == "1000"
-    assert recipe_results[0]["max_ready_time"] == "60"
+    #recipe_results = [recipe for recipe in results["results"]]
+    assert results[0]["title"] == "Smoked Salmon Pasta"
+    assert results[0]["servings"] == 4
+    assert results[0]["readyInMinutes"] == 45
+    assert results[0]["nutrition"]["nutrients"][0]["amount"] == 473.939
+    assert results[0]["aggregateLikes"] == 33
+    
 
 
-    invalid_results = get_recipes(type_of_meal="pasta",final_ingreds="pasta+OOPS", minimum_calories="hello", maximum_calories="hi", max_ready_time="hey")
-    assert invalid_results == None
+
+  
+
+
+   # invalid_results = get_recipes(type_of_meal="pasta",final_ingreds="pasta+OOPS", minimum_calories="hello", maximum_calories="hi", max_ready_time="hey")
+   # assert invalid_results == None
