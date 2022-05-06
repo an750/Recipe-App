7ca3605fb74bc5fc6d74b9180b1a7be3e4b07051
will-mcn <wam59@georgetown.edu>
2022-05-05 20:42:31 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-APP_ENV = os.getenv("APP_ENV", default="development") # use "production" on a remote server
+APP_ENV = os.getenv("APP_ENV", default="production") # use "production" on a remote server
